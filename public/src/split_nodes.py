22f37dd219bfa1421d937c94241e845fb81a83db
Luke Morrison <lukeivanmorrison@gmail.com>
2026-03-06 22:55:33 -0500

    Chapter 3, Lesson 1

--- public/src/split_nodes.py
+++ public/src/split_nodes.py
@@ -1,4 +1,4 @@
-from textnode import TextNode, TextType
+from text_node import TextNode, TextType
 from inline_markdown import extract_markdown_images, extract_markdown_links
 
 def split_nodes_image(old_nodes):
