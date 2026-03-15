f801e53a1492a18281e46c8914b792928cbbde97
Luke Morrison <lukeivanmorrison@gmail.com>
2026-03-15 09:39:01 -0400

    Lesson 2, Chapter 6

--- public/src/test_text_node.py
+++ public/src/test_text_node.py
@@ -40,6 +40,32 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
+    def test_bold_text(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold text node")
+
+    def test_italicized_text(self):
+        node = TextNode("This is an italicized text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is an italicized text node")
+
+    def test_image_node_text(self):
+        node = TextNode("This is an image node", TextType.IMAGE, "https://www.boot.dev/logo.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props,
+                         {"src":"https://www.boot.dev/logo.png","alt":"This is an image node"})
+
+    def test_link(self):
+        node = TextNode("Click me!", TextType.LINK, "https://www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click me!")
+        self.assertEqual(html_node.props, {"href": "https://www.google.com"})
 
 if __name__ == "__main__":
     unittest.main()
